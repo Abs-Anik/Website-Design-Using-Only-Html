82979ccbc9e5a06a2776061141dfa11ddc386ddd
Abs-Anik <abubakkarsiddik275@gmail.com>
2020-02-01 02:10:38 +0600

    complete full design

---
+++ problem solve/grade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>View Result</title>
+</head>
+<body>
+	<h2 align="center">Input Your Number and View Result</h2>
+	
+
+	<form action="result.php" method="get">
+	  		<fieldset>
+	    	<legend>Required:</legend>
+	    	<label for="number">Input Your Number</label>
+	    	<input type="text" name="firstname" value="Mickey">
+	    	<br><br>
+	    	<input type="submit" value="Submit">
+	  		</fieldset>
+	</form>
+</body>
+</html>
